7cb25ee282b401b668498ade77da0db72e26129d
Valerino Wijaya <valerino@gmail.com>
2014-07-14 17:15:15 +0700

    Lec 213. Setting target action programatically

--- A View Into The World/VWViewController.m
+++ A View Into The World/VWViewController.m
@@ -37,16 +37,29 @@
 //    NSLog(@"button 2 frame origin (%f, %f)", self.button2.frame.origin.x, self.button2.frame.origin.y);
 //    NSLog(@"button 2 center (%f, %f)", self.button2.center.x, self.button2.center.y);
 
+    // Add UIView programatically
     CGRect myViewsFrame = CGRectMake(20, 250, 200, 60);
     UIView *myView = [[UIView alloc] initWithFrame:myViewsFrame];
     myView.backgroundColor = [UIColor redColor];
     [self.view addSubview:myView];
     
+    // Add UIButton programatically
     UIButton *anotherButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     anotherButton.frame = CGRectMake(40, 40, 200, 100);
     anotherButton.backgroundColor = [UIColor greenColor];
     [anotherButton setTitle:@"Press me... Please!" forState:UIControlStateNormal];
     [self.view addSubview:anotherButton];
+    
+    // Remove UIView programatically
+    [myView removeFromSuperview];
+    
+    // Setting target action programatically
+    [anotherButton addTarget:self action:@selector(didPressButton:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+-(void)didPressButton:(UIButton *) button
+{
+    NSLog(@"I'm doing everything in code now!");
 }
 
 - (void)didReceiveMemoryWarning
