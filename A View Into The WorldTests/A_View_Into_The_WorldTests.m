e2027336af5bdf67d09647b0ff7f72e2b97a8542
Valerino Wijaya <valerino@gmail.com>
2014-07-14 12:11:52 +0700

    Initial Commit

---
+++ A View Into The WorldTests/A_View_Into_The_WorldTests.m
@@ -0,0 +1,34 @@
+//
+//  A_View_Into_The_WorldTests.m
+//  A View Into The WorldTests
+//
+//  Created by Valerino on 7/14/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface A_View_Into_The_WorldTests : XCTestCase
+
+@end
+
+@implementation A_View_Into_The_WorldTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
